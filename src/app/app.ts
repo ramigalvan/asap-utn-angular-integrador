411bec9b4d64a6ad922c97f035bdf09d42167b76
ramirogalvan <rgalvan@asap-consulting.net>
2025-10-13 11:21:13 -0300

    update(app): imports basic components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { TopNav } from './movie/components/top-nav/top-nav';
+import { MovieGrid } from './movie/components/movie-grid/movie-grid';
+import { Search } from "./movie/components/search/search";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, TopNav, Search],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.css'
 })
 export class App {
+
   protected readonly title = signal('asap-utn-angular-integrador');
+
 }
