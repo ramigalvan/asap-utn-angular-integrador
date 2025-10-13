4ffde13d2b658ca96f808cd01542c09b435f89b3
ramirogalvan <rgalvan@asap-consulting.net>
2025-10-13 11:21:58 -0300

    feat(routes): making path items

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Home } from './movie/pages/home/home';
+import { Movies } from './movie/pages/movies/movies';
+import { TvShows } from './movie/pages/tv-shows/tv-shows';
+import { Favorites } from './movie/pages/favorites/favorites';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home, pathMatch: 'full' },
+    { path: 'movies', component: Movies },
+    { path: 'tv-shows', component: TvShows },
+    { path: 'favorites', component: Favorites },
+    { path: '**', redirectTo: '' }, // redirige si la ruta no existe
+];
