313d72e8de0cb53189811e038f6f3e97a1e60061
ramirogalvan <rgalvan@asap-consulting.net>
2025-10-15 16:22:54 -0300

    feat(home-page): implement movieService

--- src/app/movie/pages/home/home.ts
+++ src/app/movie/pages/home/home.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { MovieGrid } from "../../components/movie-grid/movie-grid";
+import { Search } from "../../components/search/search";
+import {  MovieService } from '../../services/movie-service';
+import { TrendingItem } from '../../models/trending';
 
 @Component({
   selector: 'app-home',
-  imports: [MovieGrid],
+  imports: [MovieGrid, Search],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
-export class Home {
+export class Home implements OnInit {
+  movies = signal<TrendingItem[]>([])
+  movieService = inject(MovieService)
+
+
+  ngOnInit(): void {
+    this.movieService.getTrendingByAllWeek()
+      .subscribe({
+        next: (res) => (this.movies.set(res.results)),
+        error: (err) => console.error(err),
+
+      })
+  }
+
+  onSearch(query: string) {
+    // if (!query) {
+    //   this.movieService.getAll()
+    //     .subscribe(data => this.movies.set(data));
+    // } else {
+    //   this.movieService.getByTitle(query)
+    //     .subscribe(data => this.movies.set(data));
+    // }
+  }
 
 }
