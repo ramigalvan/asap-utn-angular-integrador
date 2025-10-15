cb7fc80c2462c9a607f70d39c7ba9b986471ab70
ramirogalvan <rgalvan@asap-consulting.net>
2025-10-15 16:26:09 -0300

    feat(tv-shows): add tv shows page

--- src/app/movie/pages/tv-shows/tv-shows.ts
+++ src/app/movie/pages/tv-shows/tv-shows.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Search } from "../../components/search/search";
+import { MovieGrid } from "../../components/movie-grid/movie-grid";
 
 @Component({
   selector: 'app-tv-shows',
-  imports: [],
+  imports: [Search, MovieGrid],
   templateUrl: './tv-shows.html',
   styleUrl: './tv-shows.css'
 })
