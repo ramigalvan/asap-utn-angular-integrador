e0b65c16a766ac45086fea214d9ab9056df37b4e
ramirogalvan <rgalvan@asap-consulting.net>
2025-10-24 20:27:44 -0300

    update(app): remove footer component

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,13 +2,10 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { TopNav } from './movie/components/top-nav/top-nav';
-import { MovieGrid } from './movie/components/movie-grid/movie-grid';
-import { Search } from "./movie/components/search/search";
-import { FooterInfo } from "./movie/components/footer-info/footer-info";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule, TopNav, FooterInfo],
+  imports: [RouterOutlet, FormsModule, TopNav ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
